c3b31927f3322674d5fff44c57deb6f81fca3198
mattrjacobs <mattrjacobs@gmail.com>
2013-01-13 16:57:02 -0700

    Exercise 2 (absolute)

---
+++ src/main/scala/Operation.scala
@@ -0,0 +1,21 @@
+package com.mattrjacobs.fp
+
+object Operation {
+  type Pred[A] = A => Boolean
+
+  def even(n: Int): Boolean = n % 2 == 0
+  def negative(n: Int): Boolean = n < 0
+  def odd_1(n: Int): Boolean = !(even(n))
+  def positive_1(n: Int): Boolean = !(negative(n))
+  def notMonomorphic(p: Pred[Int]): Pred[Int] =
+    n => !(p(n))
+  def not[A](p: Pred[A]): Pred[A] = n => !(p(n))
+  val odd = not[Int](even)
+  val positive = not[Int](negative)
+  def abs(n: Int) = negative(n) match {
+    case true  => -n
+    case false => n
+  }
+  def absoluteMonomorphic(p: Int => Int): Int => Int =
+    n => abs(p(n))
+}
---
+++ src/test/scala/Exercise2.scala
@@ -0,0 +1,54 @@
+package com.mattrjacobs.fp
+
+import org.specs2._
+
+class Exercise2 extends Specification {
+  def is =
+    "Exercise2" ^
+      "even" ^
+      "yes" ! evenYes ^
+      "no" ! evenNo ^
+      p ^
+      "negative" ^
+      "yes" ! negativeYes ^
+      "no" ! negativeNo ^
+      p ^
+      "odd_1" ^
+      "yes" ! odd_1Yes ^
+      "no" ! odd_1No ^
+      p ^
+      "positive_1" ^
+      "yes" ! positive_1Yes ^
+      "no" ! positive_1No ^
+      p ^
+      "odd" ^
+      "yes" ! oddYes ^
+      "no" ! oddNo ^
+      p ^
+      "positive" ^
+      "yes" ! positiveYes ^
+      "no" ! positiveNo ^
+      p ^
+      "absolute" ^
+      "cube" ^
+      "positive" ! cubePositive ^
+      "negative" ! cubeNegative
+
+  def evenYes = Operation.even(2) must beTrue
+  def evenNo = Operation.even(1) must beFalse
+  def negativeYes = Operation.negative(-9) must beTrue
+  def negativeNo = Operation.negative(6) must beFalse
+  def odd_1Yes = Operation.odd_1(5) must beTrue
+  def odd_1No = Operation.odd_1(4) must beFalse
+  def positive_1Yes = Operation.positive_1(5) must beTrue
+  def positive_1No = Operation.positive_1(-4) must beFalse
+  def oddYes = Operation.odd(3) must beTrue
+  def oddNo = Operation.odd(10) must beFalse
+  def positiveYes = Operation.positive(5) must beTrue
+  def positiveNo = Operation.positive(-9) must beFalse
+  def cubePositive = Operation.absoluteMonomorphic(cube)(3) mustEqual 27
+  def cubeNegative = Operation.absoluteMonomorphic(cube)(-3) mustEqual 27
+
+  private def cube(n: Int) = n * n * n
+
+}
