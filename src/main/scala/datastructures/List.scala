69e6944152cef9dfe590ff2e9d62d4ec5a863961
mattrjacobs <mattrjacobs@gmail.com>
2013-01-15 20:54:37 -0700

    Exercise 3_4 (dropWhile)

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -62,7 +62,11 @@ object List { // `List` companion object
     case (Cons(x, xs), _) => drop(xs, n - 1)
   }
 
-  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = sys.error("todo")
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil                 => Nil
+    case Cons(x, xs) if f(x) => dropWhile(xs)(f)
+    case l                   => l
+  }
 
   def setHead[A](l: List[A])(h: A): List[A] = sys.error("todo")
 
---
+++ src/test/scala/datastructures/Exercise3_4.scala
@@ -0,0 +1,17 @@
+package com.mattrjacobs.fp
+
+import org.specs2._
+
+class Exercise3_4 extends Specification {
+  def is =
+    "Exercise3-4" ^
+      "Nil dropWhile" ! dropWhileNil ^
+      "list dropWhile odd" ! dropWhileOdd ^
+      "list dropWhile lessThan 4" ! dropWhileLessThanFour
+
+  val l = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+  def dropWhileNil = List.dropWhile(Nil)(_ => true) must_== Nil
+  def dropWhileOdd = List.dropWhile(l)(i => i % 2 == 1) must_== Cons(2, Cons(3, Cons(4, Nil)))
+  def dropWhileLessThanFour = List.dropWhile(l)(i => i < 4) must_== Cons(4, Nil)
+}
