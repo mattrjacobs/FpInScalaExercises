a2223376a814126e409c1ef5668117e07bc5b97a
mattrjacobs <mattrjacobs@gmail.com>
2013-01-17 21:38:34 -0800

    Exercise 3_26 (Tree.size)

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -5,5 +5,8 @@ case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_)                     => 1
+    case Branch(leftTree, rightTree) => Tree.size(leftTree) + Tree.size(rightTree)
+  }
 }
