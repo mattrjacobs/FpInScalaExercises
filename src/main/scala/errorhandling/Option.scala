1f73a1434b8b3fd6a09e706ef05d8f0231c80469
mattrjacobs <mattrjacobs@gmail.com>
2013-02-24 20:21:42 -0500

    Exercise 4-1: Adding Option with worked methods in trait

---
+++ src/main/scala/errorhandling/Option.scala
@@ -0,0 +1,45 @@
+package com.mattrjacobs.fp
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(v) => Some(f(v))
+    case None    => None
+  }
+
+  def flatMap_patternMatch[B](f: A => Option[B]): Option[B] = this match {
+    case Some(v) => f(v)
+    case None    => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    this.map(f) getOrElse None
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(v) => v
+    case None    => default
+  }
+
+  def orElse_patternMatch[B >: A](ob: => Option[B]): Option[B] = this match {
+    case Some(v) => Some(v)
+    case None    => ob
+  }
+
+  def orElse[B >: A](ob: Option[B]): Option[B] =
+    this.map(Some(_)) getOrElse ob
+
+  def filter_patternMatch(f: A => Boolean): Option[A] = this match {
+    case Some(v) if f(v) => Some(v)
+    case Some(v)         => None
+    case None            => None
+  }
+
+  def filter(f: A => Boolean): Option[A] =
+    this.flatMap(v => if (f(v)) Some(v) else None)
+}
+
+case class Some[+A](v: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+
+}
