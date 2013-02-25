a98216207c098dcc94c3635385980653fd7fa1fe
mattrjacobs <mattrjacobs@gmail.com>
2013-02-24 20:54:18 -0500

    Exercise 4-3 (map2 on option)

--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -52,4 +52,49 @@ object Option {
 
     mean(xs).flatMap(varianceGivenMean(_, xs))
   }
+
+  import java.util.regex._
+
+  def pattern(s: String): Option[Pattern] =
+    try {
+      Some(Pattern.compile(s))
+    } catch {
+      case e: PatternSyntaxException => None
+    }
+
+  def mkMatcher(pat: String): Option[String => Boolean] =
+    pattern(pat) map (p => (s: String) => p.matcher(s).matches)
+
+  def mkMatcher_1(pat: String): Option[String => Boolean] =
+    for {
+      p <- pattern(pat)
+    } yield ((s: String) => p.matcher(s).matches)
+
+  def doesMatch(pat: String, s: String): Option[Boolean] =
+    for {
+      p <- mkMatcher_1(pat)
+    } yield p(s)
+
+  def bothMatch(pat: String, pat2: String, s: String): Option[Boolean] =
+    for {
+      f <- mkMatcher(pat)
+      g <- mkMatcher(pat2)
+    } yield f(s) && g(s)
+
+  def bothMatch_1(pat: String, pat2: String, s: String): Option[Boolean] =
+    mkMatcher(pat) flatMap (f =>
+      mkMatcher(pat2) map (g =>
+        f(s) && g(s)))
+
+  def map2_1[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    (a, b) match {
+      case (Some(v1), Some(v2)) => Some(f(v1, v2))
+      case _                    => None
+    }
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for {
+      v1 <- a
+      v2 <- b
+    } yield f(v1, v2)
 }
