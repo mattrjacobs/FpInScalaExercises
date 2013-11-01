1f15eacbecf6e4bdcc54f12fe0e9f0d39b8794c5
Matt Jacobs <mattrjacobs@gmail.com>
2013-11-01 09:00:56 -0700

    Exercise 6-9: RNG.map/map2 in terms of RNG.flatMap

--- src/main/scala/state/RNG.scala
+++ src/main/scala/state/RNG.scala
@@ -10,24 +10,18 @@ trait RNG {
   def unit[A](a: A): Rand[A] =
     rng => (a, rng)
 
-  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
-    rng => {
-      val (a, rng2) = s(rng)
-      (f(a), rng2)
-    }
-
   def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
     rng => {
       val (a, rng2) = f(rng)
       g(a)(rng2)
     }
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
-    rng => {
-      val (a, rng2) = ra(rng)
-      val (b, rng3) = rb(rng2)
-      (f(a, b), rng3)
-    }
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(a => unit(f(a)))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(f(a, _)))
+  }
 
   def positiveLessThan(n: Int): Rand[Int] =
     flatMap(positiveInt) { i =>
