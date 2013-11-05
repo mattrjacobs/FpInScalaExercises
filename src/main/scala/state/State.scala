0d6ea4f898ae12da26f7d4d9ccaaeea5407b07f6
Matt Jacobs <mattrjacobs@gmail.com>
2013-11-05 09:06:27 -0800

    Exercise 6-10: Abstracted pieces of RNG into general State trait

---
+++ src/main/scala/state/State.scala
@@ -0,0 +1,27 @@
+package com.mattrjacobs.fp.state
+
+case class State[S, +A](run: S => (A, S)) {
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(s => {
+      val (a, s2) = this.run(s)
+      f(a).run(s2)
+    })
+
+  def map[B](f: A => B): State[S, B] =
+    flatMap(a => State.unit(f(a)))
+
+  def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    flatMap(a => sb.map(b => f(a, b)))
+}
+
+object State {
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[A, S](fs: List[State[S, A]]): State[S, List[A]] =
+    State(s => fs.foldRight((Nil: List[A], s)) {
+      case (f, (l, s)) => {
+        val (nextValue, nextState) = f.run(s)
+        (l :+ nextValue, nextState)
+      }
+    })
+}
